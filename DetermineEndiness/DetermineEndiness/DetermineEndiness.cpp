f827697bb37c0a7c546a54b5db8f5cf9bae65608
Alexander Ivanov <alex.ivanov0722@gmail.com>
2023-03-02 20:46:48 +0200

    Update DetermineEndiness.cpp

--- DetermineEndiness/DetermineEndiness/DetermineEndiness.cpp
+++ DetermineEndiness/DetermineEndiness/DetermineEndiness.cpp
@@ -1,3 +1,4 @@
+/* Proving the machine's endianity. */
 #include <iostream>
 #include <functional>
 
@@ -5,11 +6,34 @@ using namespace std;
 
 int main()
 {
-    auto endiness = [](auto variable) -> string
+    /* Recursively check is Little endianity. */
+    /* by using Allman coding style and only one return (without early returns) (if any) */
+    auto isLittleEndian = []()
     {
-        return (*(char*)&variable) ? "little endian" : "big endian";
+        int i = 50462976; /* {0, 1, 2, 3} */
+        char cArr[] = { 0, 1, 2, 3 };
+
+        function<bool(char*, char*, char*)> _isLittleEndian = [&_isLittleEndian](char* cPI, char* cArrBegin, char* cArrEnd) -> bool
+        {
+            bool result;
+
+            if (cArrBegin > cArrEnd)
+            {
+                result = true;
+            }
+            else
+            {
+                result = (int)*cPI == (int)*cArrBegin ? _isLittleEndian(++cPI, ++cArrBegin, cArrEnd) : false;
+            }
+
+            return result;
+        };
+
+        return _isLittleEndian((char*)&i, cArr, cArr + 3);
     };
 
-    cout << endiness(5) << endl;
-    cout << endiness(5.0) << endl;
+    cout << endl << boolalpha;
+    cout << isLittleEndian();
+
+    return 0;
 }
